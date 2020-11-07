a4b975eac2b8db56be82b290b870be165fbdea19
CruzHenry56 <henry.cruz@mail.citytech.cuny.edu>
2020-11-07 14:13:08 -0500

    lab9

--- pluralize.py
+++ pluralize.py
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <script>
-    function pluralize(word){
-      return word + "s";
-    }
+word = input("Word please... ")
 
-    var userword = prompt("Word please...")
-    console.log(pluralize(userword));
-    </script>
-  </body>
-</html>
+def pluralize():
+     return word
+
+print(word +"s")
